bfdaee56ae9ca61958389f310c7746373c4014bc
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-14 16:23:22 +0200

    hw 11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // npm i 
 // npm install axios
 //npm i notiflix
+// index html
 import Notiflix from 'notiflix';
 import axios from 'axios';
+//http запити 
+// key (required)	str	Your API key: 40669349-8b3833151bb6b3e237d7939fc
