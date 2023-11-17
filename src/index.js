d062f923690a5456920b69db69a689d6eef408ca
Nadya <timrova.nadiia1622@vu.cdu.edu.ua>
2023-11-17 19:02:15 +0200

    homework 11

--- src/index.js
+++ src/index.js
@@ -1,8 +1,138 @@
-// npm i 
-// npm install axios
-//npm i notiflix
-// index html
 import Notiflix from 'notiflix';
 import axios from 'axios';
-//http запити 
-// key (required)	str	Your API key: 40669349-8b3833151bb6b3e237d7939fc
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+//const apiKey = '40669349-8b3833151bb6b3e237d7939fc';
+let currentPage = 1;
+let currentQuery = '';
+const perPage = 40;
+const apiKey = '40669349-8b3833151bb6b3e237d7939fc';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const searchForm = document.getElementById('search-form');
+const galleryContainer = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+
+const lightbox = new SimpleLightbox('.photo-card', {
+  disableScroll: false,
+  history: false,
+  nextOnImageClick: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+
+const fetchData = async (query, page = 1) => {
+  const response = await axios.get(BASE_URL, {
+    params: {
+      key: apiKey,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: page,
+      per_page: perPage,
+    },
+  });
+  return response.data;
+};
+
+const renderGallery = images => {
+  if (images.length === 0) {
+    Notiflix.Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+    return;
+  }
+
+  const galleryMarkup = images
+    .map(image => {
+      return `
+          <a href="${image.largeImageURL}" class="photo-card">
+            <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item"><b>Likes:</b> ${image.likes}</p>
+              <p class="info-item"><b>Views:</b> ${image.views}</p>
+              <p class="info-item"><b>Comments:</b> ${image.comments}</p>
+              <p class="info-item"><b>Downloads:</b> ${image.downloads}</p>
+            </div>
+          </a>
+        `;
+    })
+    .join('');
+
+  if (currentPage === 1) {
+    galleryContainer.innerHTML = galleryMarkup;
+  } else {
+    galleryContainer.innerHTML += galleryMarkup;
+  }
+
+  const { height: cardHeight } =
+    galleryContainer.firstElementChild.getBoundingClientRect(); 
+    //getBoundingClientRect() повертає розміри та позицію елемента відносно видимої області вікна.
+  window.scrollBy({ top: cardHeight , behavior: 'smooth' });
+
+  lightbox.refresh();
+};
+const handleFormSubmit = async event => {
+  event.preventDefault();
+  try {
+    currentQuery = event.target.elements.searchQuery.value.trim();
+//Отримує значення текстового поля з ім'ям searchQuery від форми та прибирає зайві пробіли з початку і кінця рядка. 
+//Значення введене користувачем стає поточним запитом пошуку.
+    if (!currentQuery) {
+      Notiflix.Notify.warning('Please enter a search query.');
+      return;
+    }
+
+    currentPage = 1;
+    
+    
+
+    const data = await fetchData(currentQuery, currentPage);
+
+    if (data && data.hits.length > 0) { //Перевіряє, чи отримані дані не є порожніми і чи є в них зображення
+      renderGallery(data.hits);
+      if (data.hits.length < perPage) {
+        loadMoreBtn.style.display = 'none';
+      } else {
+        loadMoreBtn.style.display = 'block';
+      }
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    } else {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+const handleLoadMoreClick = async () => {
+  try {
+    currentPage++;
+    loadMoreBtn.disabled = true;
+
+    const data = await fetchData(currentQuery, currentPage);
+
+    if (data && data.hits.length > 0) {
+      renderGallery(data.hits);
+    } else {
+      loadMoreBtn.style.display = 'none';
+      Notiflix.Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    loadMoreBtn.disabled = false;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+searchForm.addEventListener('submit', handleFormSubmit);
+loadMoreBtn.addEventListener('click', handleLoadMoreClick);
